b1dfd8b4bec0c7b3e722cfb6903075a71e625e92
Mateus de Sousa <mateus27sousa@outlook.com>
2018-11-01 02:42:02 -0300

    Alteração de todo o arquivo de conexão com o BD

--- config/config.php
+++ config/config.php
@@ -1,17 +1,9 @@
 <?php
-ob_start();
-
 // configura fuso horario padrao
 date_default_timezone_set('America/Sao_Paulo');
 
-// credenciais do banco
-define('DBHOST', 'localhost');
-define('DBUSER', 'root');
-define('DBPASS', '');
-define('DBNAME', 'db_guardian'); //define('DBNAME', 'nome_do_banco');
 
-
-try {
+/*try {
     //cria conexão PDO
     $db = new PDO("mysql:host=" . DBHOST . ";charset=utf8mb4;dbname=" . DBNAME, DBUSER, DBPASS);
     //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);// descomentar em produção
@@ -23,12 +15,22 @@ try {
     echo '<p class="bg-danger">' . $e->getMessage() . '</p>';
     exit;
 }
-/**
+
  * Cria token CSRF para sessão
  */
 
 // Inclui classe do usuario e passa na conexao do banco
-include 'classes/user.php';
-include 'classes/phpmailer/mail.php';
-$user = new User($db);
+/*include '../classes/user.php';
+include '../classes/phpmailer/mail.php';
+$user = new User($db);*/
+
+//conexão simples com o banco
+
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "teste";
+
+//Criar conexão
+$conex = mysqli_connect($servidor, $usuario, $senha, $dbname);
 ?>
