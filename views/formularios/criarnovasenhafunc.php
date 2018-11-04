6fa6c0f1c133cc4e159aac87d4f64eb1f57477a3
Mateus de Sousa <Mateusdevsousa@gmail.com>
2018-11-04 17:47:23 -0300

    realocação e criação de arquivos de redefinição de senha dos usuarios

--- views/formularios/criarnovasenhafunc.php
+++ views/formularios/criarnovasenhafunc.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>GTela de redefinição</title>
-    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="../../css/style.css" type="text/css">
     <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 
 </head>
@@ -22,7 +22,7 @@
   ?>
     <div class="login-page">
         <div class="form">
-          <form class="login-form" method="POST" action="php_utilities/cliente/novasenhacliente.php">
+          <form class="login-form" method="POST" action="../../php_utilities/funcionario/novasenhafunc.php">
             <input type="hidden" name="chave" value="<?php echo $chave; ?>">
             <input type="email" name="email" placeholder="Confirme seu email"/>            
             <input type="password" name="password1" placeholder="Digite a nova senha"/>
