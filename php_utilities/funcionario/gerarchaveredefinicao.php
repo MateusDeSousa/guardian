58ca15ae74548beb437c4185ae4b1526507ccc9e
Mateus de Sousa <Mateusdevsousa@gmail.com>
2018-11-04 17:49:00 -0300

    criação de arquivos de manipulação do CRUD de funcionarios

--- php_utilities/funcionario/gerarchaveredefinicao.php
+++ php_utilities/funcionario/gerarchaveredefinicao.php
@@ -1,17 +1,18 @@
 <?php
 session_start();
 include_once '../../config/config.php';
-include_once '../../classes/funcaocliente.php';
+include_once '../../classes/funcaofuncionario.php';
 $conn = new conectar();
 
 $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
 
 //$chave = $conn->cadastrar($name, $lastName, $userName, $cnh, $emailAddress, $password);
 
+
 $chave = $conn->gerarchave($email);
 
 if ($chave) {
-    echo '<a href="http://localhost/guardian/criarnovasenha.php?chave='.$chave.'"><p align="center">http://localhost/guardian/criarnovasenha.php?chave='.$chave.'</p></a>';
+    echo '<a href="http://localhost/guardian/views/formularios/criarnovasenhafunc.php?chave='.$chave.'"><p align="center">http://localhost/guardian/views/formularios/criarnovasenhafunc.php?chave='.$chave.'</p></a>';
 } else {
     echo '<h1>Erro usuario não encontrado</h1>';
 }
