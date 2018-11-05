0817e57d18aedd3f6e5b3900d918a13dfbd31f86
Mateus de Sousa <mateus27sousa@outlook.com>
2018-11-05 20:08:53 -0200

    correção do link de redirecionamento para tela de redefinição de senha do cliente

--- views/formularios/criarnovasenhacliente.php
+++ views/formularios/criarnovasenhacliente.php
@@ -2,13 +2,13 @@
   session_start();
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="PT-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>GTela de redefinição</title>
-    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="../../css/style.css" type="text/css">
     <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 
 </head>
