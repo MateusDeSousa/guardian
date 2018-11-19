59014c2e166fbf1879d9137a155fa60747816d6c
Mateus de Sousa <Mateusdevsousa@gmail.com>
2018-11-19 00:06:32 -0300

    função cadastrar carro no sistema

--- Controllers/carro/cadastrarCarro.php
+++ Controllers/carro/cadastrarCarro.php
@@ -1,40 +1,31 @@
 <?php
-    include_once '../config/config.php';
+    include_once '../../Config/config.php';
+    include_once '../../Models/carro/funcaocarro.php';
+    $conn = new conectar();
 
     $car_brand = strtolower($_POST['car_brand']);
     $car_model = strtolower($_POST['car_model']);
     $year = $_POST['year'];
-    $seats_qtd = $_POST['seats_qtd'];
+    $seat_qtd = $_POST['seat_qtd'];
     $door_qtd = $_POST['door_qtd'];
     $air_cond = $_POST['air_cond'];
-
-    if(!isset($_POST['air_cond_true'])){
-        if(!isset($_POST['air_cond_false'])){
-            echo 'Informe se o carro tem ou não, ar condicionado e tente novamente.';
-            echo '<meta HTTP-EQUIV="Refresh" CONTENT="3;URL=../views/cadastrar_carro.html">';
-            exit();
-        }
-    }
-
-    if(isset($_POST['air_cond_true'])){
-        $air_cond = 'yes';
-    } elseif (isset($_POST['air_cond_false'])){
-        $air_cond = 'no';
-    }
-
-    $sql = "INSERT INTO tb_veiculo(car_brand, car_model, year, seats_qtd, air_cond, door_qtd, status)";
-    $sql .= "VALUES ('{$car_brand}', '{$car_model}', '{$year}', '{$seats_qtd}', '{$air_cond}', '{$door_qtd}', 'disponivel')";
-
-    $query = mysqli_query($conex, $sql);
+    $abs = $_POST['abs'];
+    $sound = $_POST['sound'];
+    $status = $_POST['status'];
+    $chassis = $_POST['chassis'];
+    $board = $_POST['board'];
+    $price = $_POST['price'];
     
-    var_dump($query);
+    //Chama a função para cadastrar no banco
+    $cadastrar = $conn->cadastrar($car_brand, $car_model, $year, $seat_qtd, $air_cond, $abs, $sound, $door_qtd, $status, $chassis, $price);
 
-    if(!var_dump($query)) {
-        header('location:../views/index.html');
+/*
+    if($cadastrar) {
+        header('location:../../Views/funcionario/dashboard.html');
         exit();
     } else {
         echo 'Ocorreu um erro ao cadastrar o carro. Tente novamente.';
-        echo '<meta HTTP-EQUIV="Refresh" CONTENT="3;URL=../views/cadastrar_carro.html">';
+        echo '<meta HTTP-EQUIV="Refresh" CONTENT="3;URL=../../Views/carro/cadastrar_carro.html">';
     }
-
+*/
 ?>
